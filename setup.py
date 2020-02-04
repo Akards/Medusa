b1fc2e0dc1aa7cc197432e84bbf2f873e42388fd
KinseyMcG <kinseymcglasson@gmail.com>
2020-02-04 16:24:12 -0600

    Added files necessary for Python package recognition

---
+++ setup.py
@@ -0,0 +1,8 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='Medusa',
+    version=version['__version__'],
+    author='Kinsey McGlasson, Adam Schlossberg, Jimmy Veloso',
+)
