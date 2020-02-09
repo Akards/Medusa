8fc0d431217e94e34bbb43dc2ab648f38e13fb78
Akards <adjschloss@gmail.com>
2020-02-09 15:20:56 -0600

    update sort.py
    more updates

--- ParallelPython/sort/sort.py
+++ ParallelPython/sort/sort.py
@@ -1,4 +1,12 @@
 import abc
 
 class Sort(metaclass=abc.ABCMeta):
-	pass
+	
+	@abstractmethod
+	def sort(self):
+		pass
+
+	def recurse(self):
+		self.sort(self) #not overriding this will result in sequential execution
+
+	items = [] #should be in the form (item, int place)
